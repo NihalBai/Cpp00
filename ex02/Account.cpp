d6b41a6d00586a65925b29777a19c4541191439d
Nihal Baidaoui <nbaidaou@c1r9p7.1337.ma>
2025-08-10 09:25:14 +0100

    ex02

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -2,6 +2,10 @@
 #include <string>
 #include <iostream>
 
+#include <vector>
+#include <algorithm>
+#include <functional>
+
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
@@ -23,35 +27,153 @@ int Account ::getNbDeposits(){  return _totalNbDeposits; }
 int Account ::getNbWithdrawals(){   return _totalNbWithdrawals; }
 int Account::getTotalAmount(){  return _totalAmount;  }
 
-void Account ::displayAccountsInfos()
+void Account::displayAccountsInfos()
 {
-    int i = 0;
-    while (i < getNbAccounts())
-    {
-        std::cout<< "nb Account "+getNbAccounts();
-        std::cout<< "Nb Deposits "+getNbDeposits();
-        std::cout<< "Nb Withdrawals "+getNbWithdrawals();
-        std::cout<< "Total Amount "+getTotalAmount();
-        i++;
-    }
-    
+    _displayTimestamp();
+    std::cout << "accounts:" << getNbAccounts()
+              << ";total:" << getTotalAmount()
+              << ";deposits:" << getNbDeposits()
+              << ";withdrawals:" << getNbWithdrawals()
+              << std::endl;
 }
+
+
+// void Account::makeDeposit(int deposit)
+// {
+//     int d =getNbDeposits();
+//     int t = getTotalAmount();
+//     d++;
+//     t+=deposit;
+// }
+// bool Account ::makeWithdrawal(int withdrawal )
+// {
+//     int w = getNbWithdrawals();
+//     int t = getTotalAmount();
+//     if(withdrawal <= t)
+//     {   t-=withdrawal;
+//         w++;
+//         return true;
+//     }
+//     return false;
+// }
 
 void Account::makeDeposit(int deposit)
 {
-    int d =getNbDeposits();
-    int t = getTotalAmount();
-    d++;
-    t+=deposit;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex
+              << ";p_amount:" << _amount
+              << ";deposit:" << deposit;
+
+    _amount += deposit;
+    _nbDeposits++;
+    _totalAmount += deposit;
+    _totalNbDeposits++;
+
+    std::cout << ";amount:" << _amount
+              << ";nb_deposits:" << _nbDeposits
+              << std::endl;
 }
-bool Account ::makeWithdrawal(int withdrawal )
+
+bool Account::makeWithdrawal(int withdrawal)
 {
-    int w = getNbWithdrawals();
-    int t = getTotalAmount();
-    if(withdrawal <= t)
-    {   t-=withdrawal;
-        w++;
-        return true;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex
+              << ";p_amount:" << _amount;
+
+    if (withdrawal > _amount) {
+        std::cout << ";withdrawal:refused" << std::endl;
+        return false;
     }
-    return false;
+
+    _amount -= withdrawal;
+    _nbWithdrawals++;
+    _totalAmount -= withdrawal;
+    _totalNbWithdrawals++;
+
+    std::cout << ";withdrawal:" << withdrawal
+              << ";amount:" << _amount
+              << ";nb_withdrawals:" << _nbWithdrawals
+              << std::endl;
+    return true;
+}
+
+int Account :: checkAmount()const
+{
+    return _amount;
+}
+
+void Account::displayStatus() const
+{
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex
+              << ";amount:" << _amount
+              << ";deposits:" << _nbDeposits
+              << ";withdrawals:" << _nbWithdrawals
+              << std::endl;
+}
+
+
+void Account::_displayTimestamp(void)
+{
+    std::cout << "[19920104_091532] ";
+}
+
+
+Account::~Account()
+{
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex
+              << ";amount:" << _amount
+              << ";closed" << std::endl;
+}
+
+
+int		main( void ) {
+
+	typedef std::vector<Account::t>							  accounts_t;
+	typedef std::vector<int>								  ints_t;
+	typedef std::pair<accounts_t::iterator, ints_t::iterator> acc_int_t;
+
+	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
+	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
+	accounts_t				accounts( amounts, amounts + amounts_size );
+	accounts_t::iterator	acc_begin	= accounts.begin();
+	accounts_t::iterator	acc_end		= accounts.end();
+
+	int	const			d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 };
+	size_t const		d_size( sizeof(d) / sizeof(int) );
+	ints_t				deposits( d, d + d_size );
+	ints_t::iterator	dep_begin	= deposits.begin();
+	ints_t::iterator	dep_end		= deposits.end();
+
+	int	const			w[]			= { 321, 34, 657, 4, 76, 275, 657, 7654 };
+	size_t const		w_size( sizeof(w) / sizeof(int) );
+	ints_t				withdrawals( w, w + w_size );
+	ints_t::iterator	wit_begin	= withdrawals.begin();
+	ints_t::iterator	wit_end		= withdrawals.end();
+
+	Account::displayAccountsInfos();
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+
+	for ( acc_int_t it( acc_begin, dep_begin );
+		  it.first != acc_end && it.second != dep_end;
+		  ++(it.first), ++(it.second) ) {
+
+		(*(it.first)).makeDeposit( *(it.second) );
+	}
+
+	Account::displayAccountsInfos();
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+
+	for ( acc_int_t it( acc_begin, wit_begin );
+		  it.first != acc_end && it.second != wit_end;
+		  ++(it.first), ++(it.second) ) {
+
+		(*(it.first)).makeWithdrawal( *(it.second) );
+	}
+
+	Account::displayAccountsInfos();
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+
+	return 0;
 }
